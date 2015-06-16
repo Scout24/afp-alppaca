d95a3b688a2f0de0e8e722cd7ce1a7bc32150c9e
Tobias Höynck <tobias.hoeynck@immobilienscout24.de>
2015-06-16 14:30:48 +0200

    Added the first happy trail test

--- src/unittest/python/ims_interface_tests.py
+++ src/unittest/python/ims_interface_tests.py
@@ -1,10 +1,19 @@
 try:
-    import unittest2
+    import unittest2 as unittest
 except ImportError:
     import unittest
 
+import requests_mock
 
-class IMSInterfaceTest(unittest2.testcase):
+from alppaca import IMSInterface
+
+
+class IMSInterfaceTest(unittest.TestCase):
     
-    def test_get_ims_credentials(self):
-        pass
+    @requests_mock.mock()
+    def test_get_ims_credentials_for_single_role(self, mock_object):
+        mock_object.get(requests_mock.ANY, text="test_role")
+        imsi = IMSInterface("http://no-such-host.com")
+        received_roles = imsi.get_roles()
+        expected_roles = ["test_role"]
+        self.assertEqual(expected_roles, received_roles)
