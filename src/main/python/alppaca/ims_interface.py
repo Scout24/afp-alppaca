4ca6be3f8f6b48a82768808eccde92e88d07dd72
Tobias Höynck <tobias.hoeynck@immobilienscout24.de>
2015-06-16 14:32:18 +0200

    Built first little dragon which eats alppacas for lunch

--- src/main/python/alppaca/ims_interface.py
+++ src/main/python/alppaca/ims_interface.py
@@ -1,22 +1,21 @@
 import requests
 
+from util import init_logging
+
+
 class IMSInterface(object):
-    def __init__(self, ims_url):
+    
+    def __init__(self, ims_url, debug=False):
         self.ims_url = ims_url
+        self.logger = init_logging(debug)
 
     def get_ims_credentials(self):
         pass
 
     def get_roles(self):
-        try:
-            response = requests.get("http://{0}/latest/meta-data/iam/security-credentials/".format(self.ims_url))
+        response = requests.get("http://{0}/latest/meta-data/iam/security-credentials/".format(self.ims_url))
 
-            roles_list = self._convert_multiline_string_to_list(response.content)
+        if response.status_code == 200:
+            roles_list = [line.strip() for line in response.content.split("\n")] if response.content else []
             self.logger.debug("Loaded roles: {0}".format(roles_list))
-            assert roles_list, "No roles supplied by upstream server"
-
             return roles_list
-        except Exception as e:
-            self.logger.error("Could not fetch roles from {0}: {1}".format(self.ims_url, e))
-
-        raise NoCredentialsException("Could not load roles from any configured metadata server")
