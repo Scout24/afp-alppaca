7717a11aa8a0188a2515c82fad70a9f106484c64
Tobias Höynck <tobias.hoeynck@immobilienscout24.de>
2015-06-16 16:00:13 +0200

    Extended get_roles() exception handling

--- src/main/python/alppaca/ims_interface.py
+++ src/main/python/alppaca/ims_interface.py
@@ -3,6 +3,10 @@ import requests
 from util import init_logging
 
 
+class NoRolesFoundException(Exception):
+    pass
+
+
 class IMSInterface(object):
     
     def __init__(self, ims_url, debug=False):
@@ -13,9 +17,18 @@ class IMSInterface(object):
         pass
 
     def get_roles(self):
-        response = requests.get("http://{0}/latest/meta-data/iam/security-credentials/".format(self.ims_url))
+        try:
+            response = requests.get("http://{0}/latest/meta-data/iam/security-credentials/".format(self.ims_url))
 
-        if response.status_code == 200:
-            roles_list = [line.strip() for line in response.content.split("\n")] if response.content else []
-            self.logger.debug("Loaded roles: {0}".format(roles_list))
-            return roles_list
+            if response.status_code == 200:
+                if not response.content:
+                    raise NoRolesFoundException("Server response was empty; host has no roles?")
+                
+                roles_list = [line.strip() for line in response.content.split("\n")] if response.content else []
+                self.logger.debug("Loaded roles: {0}".format(roles_list))
+                return roles_list
+            else:
+                response.raise_for_status()
+        except Exception as e:
+            self.logger.exception("Due to following cause:")
+            raise NoRolesFoundException(e.message)
