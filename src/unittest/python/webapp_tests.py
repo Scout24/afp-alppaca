65045583cb01a4509eaf13b44025c690093dcea0
Valentin Haenel <valentin@haenel.co>
2015-06-18 17:21:02 +0200

    fix all tests

--- src/unittest/python/webapp_tests.py
+++ src/unittest/python/webapp_tests.py
@@ -1,7 +1,7 @@
 import mock
 from webtest import TestApp
 
-from alppaca import bottle_app
+from alppaca.webapp import WebApp
 from alppaca.compat import unittest, OrderedDict
 
 json_response = '\'{"Code": "Success", ' \
@@ -15,29 +15,30 @@ json_response = '\'{"Code": "Success", ' \
 class AwsInstanceMetadataClientTest(unittest.TestCase):
 
     def setUp(self):
-        self.app = TestApp(bottle_app)
+        self.bottle_app = WebApp(mock.Mock())
+        self.app = TestApp(self.bottle_app)
 
-    @mock.patch.dict('alppaca.webapp.credentials', {'test_role': json_response})
     def test_server_is_up_and_running(self):
+        self.bottle_app.credentials = {'test_role': json_response}
         response = self.app.get('/latest/meta-data/iam/security-credentials/')
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.text, 'test_role')
 
-    @mock.patch('alppaca.webapp.credentials', OrderedDict((('test_role1', json_response), 
-                                                           ('test_role2', json_response))))
     def test_server_is_up_and_running_with_multiple_roles(self):
+        self.bottle_app.credentials = OrderedDict((('test_role1', json_response),
+                                                   ('test_role2', json_response)))
         response = self.app.get('/latest/meta-data/iam/security-credentials/')
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.text, 'test_role1\ntest_role2')
 
-    @mock.patch.dict('alppaca.webapp.credentials', {'test_role': json_response})
     def test_server_delivers_credentials_from_cache(self):
+        self.bottle_app.credentials = {'test_role': json_response}
         response = self.app.get('/latest/meta-data/iam/security-credentials/test_role')
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.text, json_response)
 
-    @mock.patch.dict('alppaca.webapp.credentials', {})
     def test_server_delivers_empty_string_on_non_existent_cache(self):
+        self.bottle_app.credentials = {}
         response = self.app.get('/latest/meta-data/iam/security-credentials/no_role')
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.text, "")
