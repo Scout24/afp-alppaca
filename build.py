67985768a15e0a83ed5125064e54f923366d442d
Tobias Höynck <tobias.hoeynck@immobilienscout24.de>
2015-06-18 14:49:56 +0200

    Reordering sequence of imports

--- build.py
+++ build.py
@@ -17,8 +17,10 @@ default_task = "publish"
 @init
 def set_properties(project):
     if sys.version_info[0:2] < (2, 7):
+        project.depends_on("ordereddict")
         project.depends_on("unittest2")
+    project.depends_on("apscheduler")
+    project.depends_on("bottle")
     project.depends_on("mock")
     project.depends_on("requests_mock")
-    project.depends_on("bottle")
     project.depends_on("webtest")
