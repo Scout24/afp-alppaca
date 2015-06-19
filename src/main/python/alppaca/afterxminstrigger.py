30fa6145806e7364899170f6f64d32901dfb8172
Tobias Höynck <tobias.hoeynck@immobilienscout24.de>
2015-06-19 17:43:47 +0200

    Adding new timed-trigger module

---
+++ src/main/python/alppaca/afterxminstrigger.py
@@ -0,0 +1,9 @@
+import datetime
+from apscheduler.triggers.date import DateTrigger
+
+
+class AfterXMinsTrigger(DateTrigger):
+    
+    def __init__(self, minutes):
+        run_date = datetime.now() + datetime.timedelta(minutes=minutes)
+        super(AfterXMinsTrigger, self).__init__(run_date=run_date)
