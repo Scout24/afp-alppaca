2984e71d9df29bb31003df89fa1cfccaae01e584
Tobias Höynck <tobias.hoeynck@immobilienscout24.de>
2015-06-23 15:06:39 +0200

    adding bin script with arguments parsing

--- src/main/python/alppaca/main.py
+++ src/main/python/alppaca/main.py
@@ -2,20 +2,15 @@ from ims_interface import IMSInterface
 from scheduler import configure_scheduler
 from webapp import WebApp
 
-local_host = '127.0.0.1'
-local_port = 5000
-ims_host = 'localhost'
-ims_port = '8080'
 
-
-def run_scheduler_and_webserver():
+def run_scheduler_and_webserver(ims_host, ims_port):
     try:
         # initialize the credentials provider
         credentials_provider = IMSInterface('{0}:{1}'.format(ims_host, ims_port))
         scheduler = configure_scheduler()
         # initialize and run the webapp
         bottle_app = WebApp(credentials_provider, scheduler)
-        bottle_app.run(host=local_host, port=local_port)
+        bottle_app.run(host='127.0.0.1', port=5000)
     except Exception, e:
         print e
 
