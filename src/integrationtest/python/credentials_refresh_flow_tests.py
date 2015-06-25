564aab18dddfb2cc6b343c339d770cf7b0447ef6
Marco Hoyer <marco@hoyer.pm>
2015-06-25 18:33:27 +0200

    running integrationtest testing roles listing endpoint and terminating the jobs accordingly

--- src/integrationtest/python/credentials_refresh_flow_tests.py
+++ src/integrationtest/python/credentials_refresh_flow_tests.py
@@ -1,6 +1,6 @@
 from alppaca.main import run_scheduler_and_webserver
 from alppaca.server_mock import MockIms
-from concurrent import futures
+from multiprocessing import Process
 import requests
 import time
 
@@ -15,13 +15,25 @@ def run_alppaca():
 
 def test_alppaca_returns_given_role():
     response = requests.get('http://localhost:5000/latest/meta-data/iam/security-credentials/')
-    print "Response was: " + response.content
-    assert(response.content == 'test_role')
+
+    assert response.status_code == 200, \
+        "Response status code should be 200, was: '{0}'".format(response.status_code)
+    assert(response.content == 'test_role'), \
+        "Response content should be 'test_role', was: '{0}'".format(response.content)
 
 
 if __name__ == '__main__':
-    executor = futures.ProcessPoolExecutor(max_workers=2)
-    mock_job = executor.submit(run_api_server_mock)
-    alppaca_job = executor.submit(run_alppaca)
-    time.sleep(2)
-    test_alppaca_returns_given_role()
+    mock_job = Process(target=run_api_server_mock)
+    alppaca_job = Process(target=run_alppaca)
+
+    mock_job.start()
+    alppaca_job.start()
+
+    try:
+        time.sleep(2)
+        test_alppaca_returns_given_role()
+    except AssertionError as e:
+        print "Test failed: {0}".format(e)
+    finally:
+        mock_job.terminate()
+        alppaca_job.terminate()
