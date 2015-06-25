c250876949a65ef559a4942ad7ac4ecad7eac19e
Marco Hoyer <marco@hoyer.pm>
2015-06-25 18:01:15 +0200

    basic concept for an integrationtest running components in parallel as tasks but not stopping them after test execution

--- src/integrationtest/python/credentials_refresh_flow_tests.py
+++ src/integrationtest/python/credentials_refresh_flow_tests.py
@@ -1,6 +1,8 @@
 from alppaca.main import run_scheduler_and_webserver
 from alppaca.server_mock import MockIms
 from concurrent import futures
+import requests
+import time
 
 
 def run_api_server_mock():
@@ -11,8 +13,15 @@ def run_alppaca():
     run_scheduler_and_webserver('../resources/alppaca_test_config.yaml')
 
 
-if __name__ == '__main__':
-    executor = futures.ProcessPoolExecutor(max_workers=1)
-    future = executor.submit(run_api_server_mock)
+def test_alppaca_returns_given_role():
+    response = requests.get('http://localhost:5000/latest/meta-data/iam/security-credentials/')
+    print "Response was: " + response.content
+    assert(response.content == 'test_role')
 
-    run_alppaca()
+
+if __name__ == '__main__':
+    executor = futures.ProcessPoolExecutor(max_workers=2)
+    mock_job = executor.submit(run_api_server_mock)
+    alppaca_job = executor.submit(run_alppaca)
+    time.sleep(2)
+    test_alppaca_returns_given_role()
