f01cf2a87bdcc602d6b88931bc12d8d4705687a8
Valentin Haenel <valentin@haenel.co>
2015-07-07 11:12:27 +0200

    refactor docstring

--- src/unittest/python/test_utils.py
+++ src/unittest/python/test_utils.py
@@ -2,8 +2,8 @@ import datetime
 
 
 class FixedDateTime(datetime.datetime):
-    """ As unfortunately datetime.datetime.now can not be mocked, since it is an c-extension.
-    We use this derived class and override now.
+    """ As unfortunately datetime.datetime.now can not be mocked, since it is
+    an c-extension.  We use this derived class and override now.
 
     http://stackoverflow.com/questions/4481954/python-trying-to-mock-datetime-date-today-but-not-working
 
