c2019f583f08bac169b4cf9ab27bb3231a99351d
Tobias Vollmer <tobias.vollmer@immobilienscout24.de>
2015-09-04 13:33:34 +0200

    flake8 fix for long line

--- src/main/python/alppaca/main.py
+++ src/main/python/alppaca/main.py
@@ -22,4 +22,5 @@ def run_scheduler_and_webserver(config_file_path):
         print e
 
 if __name__ == '__main__':
-    run_scheduler_and_webserver('src/main/python/resources/example_config.yaml')
+    configpath = 'src/main/python/resources/example_config.yaml'
+    run_scheduler_and_webserver(configpath)
