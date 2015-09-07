a9c9a1d924a687d8452ac604c8192c90684e48dd
Tobias Vollmer <tobias.vollmer@immobilienscout24.de>
2015-09-07 13:49:56 +0200

    PIO-47 Add missing function to dynamically load modules

--- src/main/python/alppaca/util.py
+++ src/main/python/alppaca/util.py
@@ -3,6 +3,24 @@ import logging
 import yaml
 
 
+def _get_item_from_module(module_name, item_name):
+    """Load classes/modules/functions/... from given config"""
+    try:
+        module = __import__(module_name, fromlist=[item_name])
+        klass = getattr(module, item_name)
+    except ImportError as error:
+        message = 'Module "{modulename}" could not be loaded: {e}'
+        raise Exception(message.format(
+            modulename=module_name, e=error))
+    except AttributeError as error:
+        message = 'No item "{itemname}" in module "{modulename}": {e}'
+        raise Exception(message.format(
+            modulename=module_name,
+            itemname=item_name,
+            e=error))
+    return klass
+
+
 def setup_logging(handler_config):
     logger = logging.getLogger('alppaca')
     if logger.handlers:
