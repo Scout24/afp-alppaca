83c7002144b6b51dc34b932facbcbd0c7d3b7cb1
heroldus <sebastian.herold@immobilienscout24.de>
2015-10-13 09:03:04 +0200

    small improvements: logging, releasing connection, formatting

--- src/main/python/alppaca/assume_role.py
+++ src/main/python/alppaca/assume_role.py
@@ -1,10 +1,13 @@
 from __future__ import print_function, absolute_import, unicode_literals, division
 
+import json
+import logging
+import socket
+
 from alppaca import NoCredentialsFoundException
 from alppaca.compat import OrderedDict
-import json
+
 from boto.sts import connect_to_region
-import socket
 
 
 class AssumedRoleCredentialsProvider():
@@ -13,30 +16,36 @@ class AssumedRoleCredentialsProvider():
         self.role_to_assume = role_to_assume
         self.aws_proxy_host = aws_proxy_host
         self.aws_proxy_port = aws_proxy_port
+        self.logger = logging.getLogger(__name__)
 
     def get_credentials_for_all_roles(self):
         original_credentials = self.credentials_provider.get_credentials_for_all_roles()
         if not original_credentials:
-            raise NoCredentialsFoundException("Got not get credentials from: " + str(self.credentials_provider))
+            raise NoCredentialsFoundException("Got no credentials from: " + str(self.credentials_provider))
 
         for role in original_credentials:
             access_key, secret_key, token = self.parse_credentials_json(original_credentials[role])
-            return self.get_assume_role_credentials(access_key, secret_key, token)
+            return self.get_credentials_for_assumed_role(access_key, secret_key, token)
 
     @staticmethod
     def parse_credentials_json(credentials_json):
         credentials_dict = json.loads(credentials_json)
         return credentials_dict['AccessKeyId'], credentials_dict['SecretAccessKey'], credentials_dict['Token']
 
-    def get_assume_role_credentials(self, access_key, secret_key, token):
+    def get_credentials_for_assumed_role(self, access_key, secret_key, token):
+        self.logger.info("Connecting to AWS region eu-central-1 ...")
         conn = connect_to_region('eu-central-1',
-                                          aws_access_key_id=access_key,
-                                          aws_secret_access_key=secret_key,
-                                          security_token=token,
-                                          proxy=self.aws_proxy_host,
-                                          proxy_port=self.aws_proxy_port
-                                          )
-        response = conn.assume_role(role_arn=self.role_to_assume, role_session_name=self.get_session_name())
+                                 aws_access_key_id=access_key,
+                                 aws_secret_access_key=secret_key,
+                                 security_token=token,
+                                 proxy=self.aws_proxy_host,
+                                 proxy_port=self.aws_proxy_port
+                                 )
+        try:
+            response = conn.assume_role(role_arn=self.role_to_assume, role_session_name=self.get_session_name())
+            self.logger.info("Successfully got credentials for role: %s", self.role_to_assume)
+        finally:
+            conn.close()
 
         results = OrderedDict()
         results[self.get_role_name()] = self.create_credentials_json(response)
