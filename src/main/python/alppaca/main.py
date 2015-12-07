2a29cb0a9e95fb1621d7a9934e51878c9c537f16
Tobias Vollmer <tobias.vollmer@immobilienscout24.de>
2015-12-07 16:49:10 +0100

    CLOUD-257 #15 Add proper logging for main application

--- src/main/python/alppaca/main.py
+++ src/main/python/alppaca/main.py
@@ -18,6 +18,7 @@ def run_scheduler_and_webserver(config):
         print("Could not setup logging with config '{0}'".format(
             logging_config), file=sys.stderr)
         raise
+    logger.info("Alppaca starting up...")
     try:
         # Credentials is a shared object that connects the scheduler and the
         # bottle_app. The scheduler writes into it and the bottle_app reads
@@ -26,10 +27,12 @@ def run_scheduler_and_webserver(config):
         # initialize the credentials provider
         ims_host_port = '%s:%s' % (config['ims_host'], config['ims_port'])
         ims_protocol = config.get('ims_protocol', 'https')
+        logger.info("Will get credentials from %s using %s", ims_host_port, ims_protocol)
         credentials_provider = IMSCredentialsProvider(ims_host_port, ims_protocol=ims_protocol)
 
         role_to_assume = config.get('assume_role')
         if role_to_assume:
+            logger.info("Option assume_role set to '%s'", role_to_assume)
             credentials_provider = AssumedRoleCredentialsProvider(
                 credentials_provider,
                 role_to_assume,
@@ -41,7 +44,10 @@ def run_scheduler_and_webserver(config):
         # initialize and run the web app
         bind_ip = config.get('bind_ip', '127.0.0.1')
         bind_port = config.get('bind_port', '25772')
+        logger.debug("Starting webserver on %s:%s", bind_ip, bind_port)
         webapp = WebApp(credentials)
         webapp.run(host=bind_ip, port=bind_port)
     except Exception:
-        logger.exception("Cannot start Alppaca")
+        logger.exception("Error in Alppaca")
+    finally:
+        logger.info("Alppaca shutting down...")
