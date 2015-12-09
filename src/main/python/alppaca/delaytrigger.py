27abfc402dfd874bb72bb61a495d169dbda75948
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2015-12-09 17:58:15 +0100

    Cloud-256, #22: Treat '0 seconds' as a normal delay
    The current backoff generator creates delays of 0 seconds. Not
    doing this causes the default behavior of the DateTrigger class to
    kick in (use local time + timezone), which is problematic with DST.

--- src/main/python/alppaca/delaytrigger.py
+++ src/main/python/alppaca/delaytrigger.py
@@ -10,7 +10,7 @@ class DelayTrigger(DateTrigger):
     """ Trigger for events to be run in X seconds. """
 
     def __init__(self, seconds):
-        if seconds > 0:
+        if seconds >= 0:
             run_date = datetime.datetime.now(pytz.utc) + datetime.timedelta(seconds=seconds)
         else:
             run_date = None
--- src/unittest/python/delaytrigger_tests.py
+++ src/unittest/python/delaytrigger_tests.py
@@ -15,15 +15,21 @@ class DelayTriggerTest(unittest.TestCase):
     @patch('datetime.datetime', FixedDateTime)
     @patch('alppaca.delaytrigger.DateTrigger.__init__')
     def test_should_compute_time_delta_for_datetrigger_for_a_given_date(self, datetrigger_mock):
-
         DelayTrigger(10)
 
         expected_run_date = datetime.datetime(1970, 1, 1, 0, 0, 10, tzinfo=pytz.utc,)
         datetrigger_mock.assert_called_with(run_date=expected_run_date)
 
+    @patch('datetime.datetime', FixedDateTime)
+    @patch('alppaca.delaytrigger.DateTrigger.__init__')
+    def test_zero_is_a_valid_delay(self, datetrigger_mock):
+        DelayTrigger(0)
+
+        expected_run_date = datetime.datetime(1970, 1, 1, 0, 0, 0, tzinfo=pytz.utc,)
+        datetrigger_mock.assert_called_with(run_date=expected_run_date)
+
     @patch('alppaca.delaytrigger.DateTrigger.__init__')
     def test_should_call_datetrigger_with_none_if_called_with_negative_delta(self, datetrigger_mock):
-
         DelayTrigger(-10)
 
         datetrigger_mock.assert_called_with(run_date=None)
