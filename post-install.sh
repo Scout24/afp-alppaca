f20e1e061eda3439dc9f20b9d434dd8f47acf83d
Tobias Vollmer <tobias.vollmer@immobilienscout24.de>
2016-02-03 10:46:43 +0100

    PIO-154: in Centos7 /var/run is a tmpfs; moving directory creation to startup script

--- post-install.sh
+++ post-install.sh
@@ -6,14 +6,6 @@
 # set alppaca to autostart
 /sbin/chkconfig alppaca on
 
-if [ ! -d /var/run/alppacad ]; then
-  if [ -e /var/run/alppacad ]; then
-    rm /var/run/alppacad
-  fi
-  mkdir -p /var/run/alppacad
-  chown alppaca:alppaca /var/run/alppacad/
-fi
-
 # Move old pid file to new place. This can be removed later on
 if [ -f /tmp/alppaca.pid ] && kill -0 $(cat /tmp/alppaca.pid); then
   mv /tmp/alppaca.pid /var/run/alppacad/alppacad.pid
