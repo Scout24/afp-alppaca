02e4cd2aa9e3d628c07cdec7c9db284437e459e0
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2016-03-22 17:43:25 +0100

    Do the threading manually instead of using APScheduler
    Instead of calling build_trigger(), the various functions now return
    the amount of seconds we can sleep.

--- src/main/python/afp_alppaca/scheduler.py
+++ src/main/python/afp_alppaca/scheduler.py
@@ -5,14 +5,11 @@ import isodate
 import json
 import logging
 import six
+import time
 from random import uniform
 
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
 from afp_alppaca.ims_interface import NoRolesFoundException
 import pytz
-
-from afp_alppaca.delaytrigger import DelayTrigger
 
 
 class Scheduler(object):
@@ -22,25 +19,12 @@ class Scheduler(object):
         update them based on the expiration date. Some randomness is involved
         to avoid collisions. In case of failure to fetch credentials a back-off
         and safety behaviour is initiated.
-
-        It is based on the apscheduler package.
     """
     def __init__(self, credentials, credentials_provider):
         self.logger = logging.getLogger(__name__)
         self.credentials = credentials
-
         self.credentials_provider = credentials_provider
         self.backoff = None
-        self.scheduler = BackgroundScheduler(timezone=pytz.utc)
-        self.scheduler.add_listener(self.job_executed_event_listener, EVENT_JOB_EXECUTED)
-        self.scheduler.add_listener(self.job_failed_event_listener, EVENT_JOB_ERROR)
-        self.scheduler.start()
-
-    def job_executed_event_listener(self, event):
-        self.logger.info("Successfully completed credentials refresh")
-
-    def job_failed_event_listener(self, event):
-        self.logger.error("Failed to refresh credentials: %s", event.exception)
 
     def do_backoff(self, factor=1.5, max_interval=10):
         """ Perform back-off and safety. """
@@ -50,9 +34,18 @@ class Scheduler(object):
                               factor, max_interval)
             self.backoff = backoff_refresh_generator(factor, max_interval)
         refresh_delta = six.next(self.backoff)
-        self.build_trigger(refresh_delta)
+        return refresh_delta
 
     def refresh_credentials(self):
+        while True:
+            try:
+                next_sleep = self._refresh_credentials()
+            except Exception:
+                self.logger.exception("Unhandled error while refreshing credentials:")
+                next_sleep = 120
+            time.sleep(next_sleep)
+
+    def _refresh_credentials(self):
         """ Refresh credentials and schedule next refresh."""
         self.logger.debug("about to fetch credentials")
         cached_credentials = None
@@ -66,10 +59,10 @@ class Scheduler(object):
             self.logger.exception("Error in credential provider:")
 
         if cached_credentials:
-            self.update_credentials(cached_credentials)
+            return self.update_credentials(cached_credentials)
         else:
             self.logger.info("No credentials found!")
-            self.do_backoff(factor=factor, max_interval=max_interval)
+            return self.do_backoff(factor=factor, max_interval=max_interval)
 
     def update_credentials(self, cached_credentials):
         """ Update credentials and retrigger refresh """
@@ -79,13 +72,13 @@ class Scheduler(object):
         refresh_delta = self.extract_refresh_delta()
         if refresh_delta < 0:
             self.logger.warn("Expiration date is in the past, entering backoff.")
-            self.do_backoff()
+            return self.do_backoff()
         else:
             if self.backoff is not None:
                 self.backoff = None
                 self.logger.debug("Exiting backoff state.")
             refresh_delta = self.sample_new_refresh_delta(refresh_delta)
-            self.build_trigger(refresh_delta)
+            return refresh_delta
 
     def extract_refresh_delta(self):
         """ Return shortest expiration time in seconds. """
@@ -98,12 +91,6 @@ class Scheduler(object):
         """ Sample a new refresh delta. """
         refresh_delta = int(uniform(refresh_delta * .5, refresh_delta * .9))
         return refresh_delta
-
-    def build_trigger(self, refresh_delta):
-        """ Actually add the trigger to the apscheduler. """
-        self.logger.debug("Setting up trigger to fire in %s seconds", refresh_delta)
-        self.scheduler.add_job(func=self.refresh_credentials, trigger=DelayTrigger(refresh_delta))
-
 
 def backoff_refresh_generator(factor, max_interval):
     """ Generate refresh deltas when in back-off and safety mode. """
