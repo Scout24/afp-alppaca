4bd5ff9d0839432a57332578866928216c247d4f
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2016-04-06 11:49:21 +0200

    Do not modify global variable DEFAULT_TEST_CONFIG
    Also, introduce setUp() to reduce boiler plate code.

--- src/integrationtest/python/logging_setup_tests.py
+++ src/integrationtest/python/logging_setup_tests.py
@@ -23,42 +23,38 @@ DEFAULT_TEST_CONFIG = {
 
 
 class RunAlppacaTests(unittest.TestCase):
-    def _helper(self, config):
-        with AlppacaIntegrationTest(config) as ait:
+    def setUp(self):
+        self.tmpfile = tempfile.NamedTemporaryFile()
+        self.config = dict(DEFAULT_TEST_CONFIG)
+        self.config['logging_handler']['args'] = [self.tmpfile.name]
+
+    def _helper(self):
+        with AlppacaIntegrationTest(self.config) as ait:
             try:
                 ait.execute()
             except AssertionError as e:
                 self.fail(e)
 
     def test_log_is_empty_when_log_level_error(self):
-        tmpfile = tempfile.NamedTemporaryFile()
-        DEFAULT_TEST_CONFIG['logging_handler']['args'] = [tmpfile.name]
-        DEFAULT_TEST_CONFIG['log_level'] = 'error'
+        self.config['log_level'] = 'error'
 
-        self._helper(DEFAULT_TEST_CONFIG)
+        self._helper()
 
-        content = tmpfile.read()
+        content = self.tmpfile.read()
         self.assertEqual(content, b'')
 
     def test_log_with_content_log_level_debug(self):
-        tmpfile = tempfile.NamedTemporaryFile()
-        DEFAULT_TEST_CONFIG['logging_handler']['args'] = [tmpfile.name]
-        DEFAULT_TEST_CONFIG['log_level'] = 'debug'
+        self._helper()
 
-        self._helper(DEFAULT_TEST_CONFIG)
-
-        content = tmpfile.read()
+        content = self.tmpfile.read()
         self.assertGreater(len(content), 0)
 
     def test_log_format_is_applied(self):
-        tmpfile = tempfile.NamedTemporaryFile()
-        DEFAULT_TEST_CONFIG['logging_handler']['args'] = [tmpfile.name]
-        DEFAULT_TEST_CONFIG['log_level'] = 'debug'
-        DEFAULT_TEST_CONFIG['log_format'] = 'foobar: hello world'
+        self.config['log_format'] = 'foobar: hello world'
 
-        self._helper(DEFAULT_TEST_CONFIG)
+        self._helper()
 
-        content = tmpfile.read()
+        content = self.tmpfile.read()
         self.assertIn(b'foobar: hello world', content)
 
 
